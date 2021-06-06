4bcaee954f7145a9c2e0d758ad898e0d79cf2fa9
djsreeraj <sreeraj011m@gmail.com>
2021-06-06 08:49:34 +0530

    fixes and  optimization

---
+++ exercise-1/src/__tests__/App.test.tsx
@@ -0,0 +1,16 @@
+import renderer from "react-test-renderer";
+import App from "../App";
+
+beforeAll(() => {
+  jest.useFakeTimers("modern");
+  jest.setSystemTime(new Date(2021, 6, 1));
+});
+
+afterAll(() => {
+  jest.useRealTimers();
+});
+
+it("renders correctly", () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
